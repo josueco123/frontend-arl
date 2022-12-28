326ad39d678dbe73c9c191f247a147b47bd1a0f4
josueco123 <45926578+josueco123@users.noreply.github.com>
2022-12-27 21:15:37 -0500

    [ARLF-2] diseño de pantallas y componentes

---
+++ src/components/ListFlies.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Avatar, List } from 'antd';
+const data = [
+  {
+    title: 'Ant Design Title 1',
+  },
+  {
+    title: 'Ant Design Title 2',
+  },
+  {
+    title: 'Ant Design Title 3',
+  },
+  {
+    title: 'Ant Design Title 4',
+  },
+];
+const ListFlies = () => (
+  <List
+    itemLayout="horizontal"
+    dataSource={data}
+    style={{ margin: '20px'}}
+    renderItem={(item) => (
+      <List.Item>
+        <List.Item.Meta
+          avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+          title={<a href="https://ant.design">{item.title}</a>}
+          description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+        />
+      </List.Item>
+    )}
+  />
+);
+export default ListFlies;
